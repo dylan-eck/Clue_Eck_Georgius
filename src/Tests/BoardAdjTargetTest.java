cf20b75110fa56b12d26fbdb6be2ca95aa4ac8d0
rlord222 <63765345+rlord222@users.noreply.github.com>
2020-10-12 23:45:03 -0600

    Added remaining test and an updated map

--- src/Tests/BoardAdjTargetTest.java
+++ src/Tests/BoardAdjTargetTest.java
@@ -1,5 +1,8 @@
 package Tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Set;
@@ -62,8 +65,6 @@ class BoardAdjTargetTest {
 		assertTrue(testList.contains(board.getCell(17, 4)));
 		assertTrue(testList.contains(board.getCell(21, 3)));
 		
-		
-		
 	}
 	
 	
@@ -100,6 +101,126 @@ class BoardAdjTargetTest {
 		assertEquals(1, testList.size());
 		assertTrue(testList.contains(board.getCell(23, 8)));
 		
+	}
+	
+	@Test
+	public void testGetTargetsGarden() {
+		
+		board.calcTargets(board.getCell(3, 3), 1);
+		Set<BoardCell> targets= board.getTargets();
+		assertEquals(2, targets.size());
+		assertTrue(targets.contains(board.getCell(7, 4)));
+		assertTrue(targets.contains(board.getCell(3, 7)));	
+		
+		board.calcTargets(board.getCell(3, 3), 2);
+		targets= board.getTargets();
+		assertEquals(6, targets.size());
+		assertTrue(targets.contains(board.getCell(7, 3)));
+		assertTrue(targets.contains(board.getCell(8, 4)));	
+		assertTrue(targets.contains(board.getCell(3, 8)));
+		assertTrue(targets.contains(board.getCell(2, 7)));	
+		
+		board.calcTargets(board.getCell(3, 3), 5);
+		targets= board.getTargets();
+		assertEquals(17, targets.size());
+		assertTrue(targets.contains(board.getCell(6, 7)));
+		assertTrue(targets.contains(board.getCell(0, 8)));	
+		assertTrue(targets.contains(board.getCell(7, 0)));
+		assertTrue(targets.contains(board.getCell(8, 7)));
+		//The two rooms
+		assertTrue(targets.contains(board.getCell(12, 3)));
+		assertTrue(targets.contains(board.getCell(3, 12)));
+		
+	}
+	
+	@Test
+	public void testgetTargetCourtYard() {
+
+		board.calcTargets(board.getCell(12, 12), 1);
+		Set<BoardCell> targets= board.getTargets();
+		assertEquals(4, targets.size());
+		assertTrue(targets.contains(board.getCell(12, 8)));
+		assertTrue(targets.contains(board.getCell(8, 12)));	
+		
+		board.calcTargets(board.getCell(12, 12), 2);
+		targets= board.getTargets();
+		assertEquals(12, targets.size());
+		assertTrue(targets.contains(board.getCell(12, 7)));
+		assertTrue(targets.contains(board.getCell(8, 11)));	
+		assertTrue(targets.contains(board.getCell(11, 16)));
+		assertTrue(targets.contains(board.getCell(13, 16)));	
+		assertTrue(targets.contains(board.getCell(16, 11)));
+
+		board.calcTargets(board.getCell(12, 12), 4);
+		targets= board.getTargets();
+		assertEquals(23, targets.size());
+		assertTrue(targets.contains(board.getCell(8, 9)));
+		assertTrue(targets.contains(board.getCell(7, 12)));	
+		assertTrue(targets.contains(board.getCell(11, 16)));
+		assertTrue(targets.contains(board.getCell(16, 15)));	
+		//two rooms it can get to
+		assertTrue(targets.contains(board.getCell(12, 3)));
+		assertTrue(targets.contains(board.getCell(12, 21)));	
+	}
+	
+	@Test
+	public void testGetTargets() {
+		
+		board.calcTargets(board.getCell(15, 9), 1);
+		Set<BoardCell> targets= board.getTargets();
+		assertEquals(2, targets.size());
+		assertTrue(targets.contains(board.getCell(15, 8)));
+		assertTrue(targets.contains(board.getCell(16, 9)));	
+	
+		board.calcTargets(board.getCell(15, 9), 2);
+		targets= board.getTargets();
+		assertEquals(5, targets.size());
+		assertTrue(targets.contains(board.getCell(15, 7)));
+		assertTrue(targets.contains(board.getCell(16, 8)));	
+		assertTrue(targets.contains(board.getCell(16, 10)));	
+		
+		board.calcTargets(board.getCell(15, 9), 5);
+		targets= board.getTargets();
+		assertEquals(17, targets.size());
+		assertTrue(targets.contains(board.getCell(16, 5)));
+		assertTrue(targets.contains(board.getCell(18, 7)));
+		assertTrue(targets.contains(board.getCell(16, 13)));	
+		assertTrue(targets.contains(board.getCell(15, 18)));	
+		assertTrue(targets.contains(board.getCell(11, 8)));	
+		//test courtyard
+		assertTrue(targets.contains(board.getCell(12, 12)));	
+		
+	}
+	
+	@Test
+	public void testGetTargetsWithOccupied() {
+
+		//test someone compleatly blocked in
+		board.getCell(8, 23).setOccupied(true);
+		board.calcTargets(board.getCell(8, 24), 1);
+		board.getCell(15, 7).setOccupied(false);
+		Set<BoardCell> targets = board.getTargets();
+		assertEquals(0, targets.size());
+		
+		board.getCell(8, 23).setOccupied(true);
+		board.calcTargets(board.getCell(12, 20), 2);
+		board.getCell(8, 23).setOccupied(false);
+		targets= board.getTargets();
+		assertEquals(2, targets.size());
+		assertTrue(targets.contains(board.getCell(8, 22)));
+		assertTrue(targets.contains(board.getCell(7, 23)));	
+
+		board.getCell(17, 21).setOccupied(true);
+		board.getCell(19, 19).setOccupied(true);
+		board.calcTargets(board.getCell(21, 21), 2);
+		board.getCell(19, 19).setOccupied(false);
+		board.getCell(17, 21).setOccupied(false);
+		targets= board.getTargets();
+		assertEquals(6, targets.size());
+		assertTrue(targets.contains(board.getCell(17, 22)));
+		assertTrue(targets.contains(board.getCell(16, 21)));
+		assertTrue(targets.contains(board.getCell(20, 17)));
+		assertTrue(targets.contains(board.getCell(19, 16)));
 	}
 
 }
